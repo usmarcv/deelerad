01533d2b4efc6262eee7974598264867467c63ea
Márcus Vinícius <usmarc.v@gmail.com>
2025-01-01 09:53:36 -0400

    feat: addinng argument epochs to main script

--- script.sh
+++ script.sh
@@ -6,11 +6,14 @@ list_model=("VGG16" "ResNet50V2" "DenseNet121" "DenseNet201" "InceptionV3" "Effi
 #Range for num_deep_radiomics
 num_deep_radiomics=("100" "300" "500")
 
+#Number of epochs
+epochs=100
+
 #Loop model
 for model in "${list_model[@]}"; do
     #Num deep radiomics
     for num in "${num_deep_radiomics[@]}"; do
         #Execute the Python command with the desired arguments
-        python3 main.py --model_name "$model" --num_deep_radiomics "$num"
+        python3 main.py --model_name "$model" --num_deep_radiomics "$num" --epochs "$epochs"
     done
 done
